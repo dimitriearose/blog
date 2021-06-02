60edda29574fe4617077880f125b047e295b215f
dimitriearose <dimitri.ea.rose@gmail.com>
2021-06-02 19:45:15 -0400

    nav items

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -3,10 +3,16 @@ import Link from "next/link"
 
 function Navbar() {
   return (
-    <div className='xl:container xl:mx-auto'>
+    <div className='xl:container xl:mx-auto flex justify-between'>
       <h1 className={styles.header}>
         <Link href='/'>D // R</Link>
       </h1>
+      <div className='flex'>
+        <div className='nav__link'>About</div>
+        <div className='nav__link'>Portfolio</div>
+        <div className='nav__link'>Blog</div>
+        <div className='nav__link'>Contact</div>
+      </div>
     </div>
   )
 }
