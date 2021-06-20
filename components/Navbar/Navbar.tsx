0a6a5f886fb4e9dd56609336b5d57c4cb4551fb1
dimitriearose <dimitri.ea.rose@gmail.com>
2021-06-20 19:59:39 -0400

    font changed

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,12 +1,13 @@
 import styles from "./Navbar.module.scss"
 import Link from "next/link"
-import TwitterIcon from '@material-ui/icons/Twitter'
+
+
 
 function Navbar() {
   return (
     <div className='xl:container xl:mx-auto flex justify-between'>
       <h1 className={styles.header}>
-        <Link href='/'>D // R</Link>
+        <Link href='/'>D//R</Link>
       </h1>
       <div className=''>
         <ul className={styles.navbar}>
@@ -15,6 +16,9 @@ function Navbar() {
           <li className={styles.nav__link}><Link href='/'>Blog</Link></li>
           <li className={styles.nav__link}><Link href='/'>Contact</Link></li>
         </ul>
+        <div className={styles.nav_mobile}>
+          
+        </div>
       </div>
     </div>
   )
