792888502bbc7c2b7a165a20e43c7b936cad45a3
dimitriearose <dimitri.ea.rose@gmail.com>
2021-06-20 21:02:55 -0400

    sidebar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,33 +1,83 @@
-import {useState} from 'react'
+import {useState,useRef} from 'react'
 import styles from "./Navbar.module.scss"
 import Link from "next/link"
 import Sidebar from '../Sidebar/Sidebar'
+import gsap from 'gsap'
+
+import styled from 'styled-components'
+
 
 function Navbar() {
 
-  const [isSidebarOpen,setIsSidebarOpen] = useState<boolean>(true)
+  const [isSidebarOpen,setIsSidebarOpen] = useState<boolean>(false)
+
+  const lineOneRef = useRef<HTMLDivElement>(null)
+  const lineTwoRef = useRef<HTMLDivElement>(null)
+
+  const onMenuOpen =  () => {
+    if (isSidebarOpen){
+      setIsSidebarOpen(false)
+    }else {
+      setIsSidebarOpen(true)
+      
+    }
+  }
+
 
   return ( 
     <>
-    <div className='xl:container xl:mx-auto flex justify-between'>
-      <h1 className={styles.header}>
+    <StyledNavbar>
+      <Header>
         <Link href='/'>D//R</Link>
-      </h1>
-      <div className=''>
-        <ul className={styles.navbar}>
-          <li className={styles.nav__link}><Link href='/about'>About</Link></li>
-          <li className={styles.nav__link}><Link href='/'>Portfolio</Link></li>
-          <li className={styles.nav__link}><Link href='/'>Blog</Link></li>
-          <li className={styles.nav__link}><Link href='/'>Contact</Link></li>
-        </ul>
-        <div className={styles.nav_mobile}>
-          
-        </div>
-      </div>
-    </div>
+      </Header>
+      <Menu aria-role='button' onClick={onMenuOpen}>
+        <div ref={lineOneRef}/>
+        <div ref={lineTwoRef}/>
+      </Menu>
+    </StyledNavbar>
        <Sidebar isOpen={isSidebarOpen} />
     </>
   )
 }
+
+const Header = styled.h2`
+  font-family: "Roboto", sans-serif;
+  font-size: 32px;
+  color: var(--secondary-color);
+  transition: all 1s ease-in-out;
+  font-family: Absender;
+  user-select:none;
+
+
+  &:hover {
+    color: var(--primary-color);
+  }
+
+`
+
+const StyledNavbar = styled.div`
+  display:flex;
+  justify-content: space-between;
+  max-width:1366px;
+  align-items: center;
+  padding: 2rem;
+`
+
+const Menu = styled.div`
+max-height: 30px;
+z-index: 10000;
+cursor:pointer;
+
+div {
+  height: 3px;
+  background-color: #000;
+  width: 30px;
+}
+
+div:first-of-type {
+  margin-bottom:5px;
+}
+
+`
 
 export default Navbar
--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -2,38 +2,69 @@ import { useState } from 'react'
 import styles from './Sidebar.module.scss'
 import Link from 'next/link'
 import styled from 'styled-components'
+import MenuIcon from '@material-ui/icons/Menu';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import {Instagram} from "@material-ui/icons";
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 interface Props {
     isOpen:boolean
 }
 
 function Sidebar({isOpen}:Props) {
-    
     return (
         <StyledSideBar isOpen={isOpen}>
-            <Link href="/about">About</Link>
-            <Link href="/portfolio">Portfolio</Link>
-            <Link href="/blog">Blog</Link>
-            <Link href="/contact">Contact</Link>
+            <TopSideBar>
+                <Link href="/about">About</Link>
+                <Link href="/portfolio">Portfolio</Link>
+                <Link href="/blog">Blog</Link>
+                <Link href="/contact">Contact</Link>
+            </TopSideBar>    
+            <BottomSideBar>
+                <a target='_blank' href='' rel='noreferrer noopener'> <TwitterIcon/></a>
+                <a target="_blank"   href='' rel='noreferrer noopener'><LinkedInIcon/></a>
+                <a target="_blank"  href='' rel='noreferrer noopener'><Instagram/></a>
+                <a target="_blank"  href='' rel='noreferrer noopener'><GitHubIcon/></a>
+            </BottomSideBar>        
         </StyledSideBar>
     )
 }
 
+const TopSideBar = styled.div`
+display: flex;
+flex-direction: column;
+flex: 1;
+`
+
+const BottomSideBar = styled.div`
+a {
+    margin-right: 10px;
+}
+`
 
 const StyledSideBar = styled.div<Props>`
     position: fixed;
-    width: 300px;
+    width: 350px;
+    height:100vh;
+    z-index: 5;
     right: 0;
     top: 0;
+    background-color: #fff;
+    padding: 2rem;
     transition: 0.3s ease all;
-    transform: ${(props) => props.isOpen ? 'translateX(300px)' : 'translateX(0px)' };
+    transform: ${(props) => props.isOpen ? 'translateX(0px)' : 'translateX(350px)' };
+    display: flex;
+    flex-direction: column;
 
     a {
-        font-size: 12px;
+        font-size: 20px;
         color :var(--secondary-color);
+        margin-bottom: 20px;
+        transition: 0.5s ease-ease-in-out;
 
        &:hover {
-           color: #000;
+        color :var(--primary-color);;
        }   
     }
 
