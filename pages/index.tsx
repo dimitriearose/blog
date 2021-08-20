b527f29b21e7145472a4b067c4e2276f64d48784
dimitriearose <dimitri.ea.rose@gmail.com>
2021-08-20 16:36:45 -0400

    contentful working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,31 @@
-import {useState,useEffect} from 'react'
+import { useState, useEffect } from "react"
 import Head from "next/head"
 import Navbar from "../components/Navbar/Navbar"
 import styles from "../styles/Home.module.scss"
-import Typewriter from 'react-simple-typewriter'
-import 'react-simple-typewriter/dist/index.css'
-import TwitterIcon from '@material-ui/icons/Twitter';
-import LinkedInIcon from '@material-ui/icons/LinkedIn'
-import {Instagram} from "@material-ui/icons";
-import GitHubIcon from '@material-ui/icons/GitHub';
+import Typewriter from "react-simple-typewriter"
+import "react-simple-typewriter/dist/index.css"
+import TwitterIcon from "@material-ui/icons/Twitter"
+import LinkedInIcon from "@material-ui/icons/LinkedIn"
+import { Instagram } from "@material-ui/icons"
+import GitHubIcon from "@material-ui/icons/GitHub"
 
-import styled from 'styled-components'
+import styled from "styled-components"
 
 export default function Home() {
-  const [windowWidth,setWindowWidth] = useState<number>(0)
+  const [windowWidth, setWindowWidth] = useState<number>(0)
 
-const onResize = () => {
-setWindowWidth(window.innerWidth)
-}
+  const onResize = () => {
+    setWindowWidth(window.innerWidth)
+  }
 
+  useEffect(() => {
+    setWindowWidth(window.innerWidth)
+    window.addEventListener("resize", onResize)
 
-useEffect(() => {
-setWindowWidth(window.innerWidth)
-window.addEventListener('resize',onResize)
-
-
-return () => {
-window.removeEventListener('resize',onResize)
-}
-},[])
+    return () => {
+      window.removeEventListener("resize", onResize)
+    }
+  }, [])
 
   return (
     <div className={styles.wrapper}>
@@ -40,76 +38,93 @@ window.removeEventListener('resize',onResize)
 
         <Navbar />
         <div className={styles.body}>
-            <h1 className={styles.body__header}>Dimitri Rose{`${windowWidth < 700 ? '' : ':'}`}</h1>
-            <TypeWriterContainer>
-              <Typewriter
-                  loop
-                  cursor
-                  cursorStyle='_'
-                  typeSpeed={70}
-                  deleteSpeed={50}
-                  delaySpeed={1000}
-                  words={['Engineer', 'Designer', 'Content Creator']}
-              />
-            </TypeWriterContainer>
+          <h1 className={styles.body__header}>
+            Dimitri Rose{`${windowWidth < 700 ? "" : ":"}`}
+          </h1>
+          <TypeWriterContainer>
+            <Typewriter
+              loop
+              cursor
+              cursorStyle='_'
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+              words={["Engineer", "Designer", "Content Creator"]}
+            />
+          </TypeWriterContainer>
         </div>
-          <ButtonContainer className="pt-24 m-auto">
-              <a href="https://twitter.com/dimitriearose" target="_blank" rel='noreferrer noopener' className={styles.icons__icon}>
-                  <TwitterIcon />
-              </a>
-              <a href="https://www.linkedin.com/in/dimitri-rose-962211188/" rel='noreferrer noopener'  target="_blank" className={styles.icons__icon}>
-                  <LinkedInIcon />
-              </a>
-              <a href="https://instagram.com/dimitriearose" target="_blank" rel='noreferrer noopener'  className={styles.icons__icon}>
-                  <Instagram />
-              </a>
-              <a href="https://github.com/dimitriearose" target="_blank" rel='noreferrer noopener' className={styles.icons__icon}>
-                  <GitHubIcon />
-              </a>
-          </ButtonContainer>
+        <ButtonContainer className='pt-24 m-auto'>
+          <a
+            href='https://twitter.com/dimitriearose'
+            target='_blank'
+            rel='noreferrer noopener'
+            className={styles.icons__icon}>
+            <TwitterIcon />
+          </a>
+          <a
+            href='https://www.linkedin.com/in/dimitri-rose-962211188/'
+            rel='noreferrer noopener'
+            target='_blank'
+            className={styles.icons__icon}>
+            <LinkedInIcon />
+          </a>
+          <a
+            href='https://instagram.com/dimitriearose'
+            target='_blank'
+            rel='noreferrer noopener'
+            className={styles.icons__icon}>
+            <Instagram />
+          </a>
+          <a
+            href='https://github.com/dimitriearose'
+            target='_blank'
+            rel='noreferrer noopener'
+            className={styles.icons__icon}>
+            <GitHubIcon />
+          </a>
+        </ButtonContainer>
       </div>
     </div>
   )
 }
 
-
 const TypeWriterContainer = styled.div`
-  @media(max-width: 1080px) {
-      padding-left: 60px;
-      padding-bottom: 30px;
-    }
+  @media (max-width: 1080px) {
+    padding-left: 60px;
+    padding-bottom: 30px;
+  }
 
-  @media(max-width: 900px) {
-  padding-left: 60px;
-}
+  @media (max-width: 900px) {
+    padding-left: 60px;
+  }
 
-  @media(max-width: 500px) {
-  display:flex;
-  justify-content:flex-start;
-  align-items:center;
- }
+  @media (max-width: 500px) {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+  }
 `
 
 const ButtonContainer = styled.div`
   a:hover {
-    color: var(--primary-color);;
+    color: var(--primary-color);
   }
-  @media(max-width: 1080px) {
-      padding-left: 60px;
-      padding-bottom: 30px;
+  @media (max-width: 1080px) {
+    padding-left: 60px;
+    padding-bottom: 30px;
   }
 
-  @media(max-width: 900px) {
+  @media (max-width: 900px) {
     display: none;
   }
 
-  @media(max-width: 850px) {
+  @media (max-width: 850px) {
     display: block;
     margin: auto;
     padding-left: 60px;
   }
 
-  @media(max-width: 500px) {
+  @media (max-width: 500px) {
     display: none;
   }
 `
--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,45 +1,57 @@
-import {useEffect, useState} from 'react'
-import Navbar from '../../components/Navbar/Navbar'
-import Post from '../../components/Post/Post'
-import Link from 'next/link'
-import styled from 'styled-components'
+import { useEffect, useState } from "react"
+import Navbar from "../../components/Navbar/Navbar"
+import Post from "../../components/Post/Post"
+import Link from "next/link"
+import styled from "styled-components"
 
-const client = require('contentful').createClient({
-    space: 's6ebgzome5xq',
-    accessToken: 'PFcScn7p6B7BPzn5i3DScSwcCGwKVjeSvCO9sqXJw6k'
+const client = require("contentful").createClient({
+  space: "s6ebgzome5xq",
+  accessToken: "PFcScn7p6B7BPzn5i3DScSwcCGwKVjeSvCO9sqXJw6k",
 })
 
 function Index() {
-    async function fetchEntries() {
-        const entries = await client.getEntries({
-            'content_type': 'portfolio',
-            'limit': 3
+  async function fetchEntries() {
+    const entries = await client.getEntries({
+      content_type: "portfolio",
+      limit: 3,
+    })
+    if (entries.items) {
+      console.log(entries.items[0].fields.title)
+      return entries.items
+    }
+  }
 
-        })
-        if (entries.items) {
-            return entries.items
-        }
-      }
+  const [posts, setPosts] = useState([])
 
-      const [posts, setPosts] = useState([])
+  useEffect(() => {
+    async function getPosts() {
+      const allPosts = await fetchEntries()
+      //@ts-ignore
+      setPosts([...allPosts])
+      //   console.log(posts)
+    }
+    getPosts()
+      .then(() => console.log("loaded posts"))
+      .catch(() => console.log("FAILED TO LOAD POSTS"))
+  }, [])
 
-      useEffect(() => {
-        async function getPosts() {
-          const allPosts = await fetchEntries()
-          //@ts-ignore
-          setPosts([...allPosts])
-            console.log(allPosts)
-        }
-        getPosts().then(() => console.log('loaded posts') )
-    }, [])
+  posts.forEach((post: any) => {
+    console.log(post.fields.title)
+  })
 
-    return (
-        <Wrapper>
-            <Container>
-                <Navbar />
-                <Header>My Recent Work:</Header>
-                <PostContainer>
-                    <Post
+  return (
+    <Wrapper>
+      <Container>
+        <Navbar />
+        <Header>My Recent Work:</Header>
+        <PostContainer>
+          <Post
+            name='weBlack'
+            image='https://avatars.githubusercontent.com/u/60557916?v=4'
+            description='This is a social media site I am currently working on.'
+            href={`/portfolio/test`}
+          />
+          {/* <Post
                         name="weBlack"
                         image="https://avatars.githubusercontent.com/u/60557916?v=4"
                         description="This is a social media site I am currently working on."
@@ -50,52 +62,44 @@ function Index() {
                         image="https://avatars.githubusercontent.com/u/60557916?v=4"
                         description="This is a social media site I am currently working on."
                         href={`/portfolio/test`}
-                    />
-                    <Post
-                        name="weBlack"
-                        image="https://avatars.githubusercontent.com/u/60557916?v=4"
-                        description="This is a social media site I am currently working on."
-                        href={`/portfolio/test`}
-                    />
-                </PostContainer>
-                <SeeAll>
-                    <Link href='/portfolio/all'>See All My Work</Link>
-                </SeeAll>
-            </Container>
-        </Wrapper>
-    )
+                    /> */}
+        </PostContainer>
+        <SeeAll>
+          <Link href='/portfolio/all'>See All My Work</Link>
+        </SeeAll>
+      </Container>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.div`
-background-color: var(--grey-color);
+  background-color: var(--grey-color);
 `
 
 const Container = styled.div`
-max-width: 1000px;
-margin-left: auto;
-margin-right: auto;
-min-height: 100vh;
-@media(max-width: 900px) {
+  max-width: 1000px;
+  margin-left: auto;
+  margin-right: auto;
+  min-height: 100vh;
+  @media (max-width: 900px) {
     padding-left: 20px;
     padding-right: 20px;
-}
+  }
 `
 const Header = styled.h1`
-font-family: Absender, sans-serif;
-font-size: 40px;
-text-align: center;
+  font-family: Absender, sans-serif;
+  font-size: 40px;
+  text-align: center;
   padding-bottom: 30px;
-color: var(--primary-color);
+  color: var(--primary-color);
 `
 
 const PostContainer = styled.div`
   width: 100%;
   display: grid;
-  grid-template-columns: auto auto auto ;
+  grid-template-columns: auto auto auto;
 `
 
-const SeeAll = styled.button`
-    
-`
+const SeeAll = styled.button``
 
 export default Index
